fc3ffecaf277d12d2da2922c089d582e37532980
Ronei Simoes <ronei.simoes19@gmail.com>
2020-04-05 18:05:04 -0300

    ATUALIZADO
    ASAE10

--- cadastro_clientes/routes/web.php
+++ cadastro_clientes/routes/web.php
@@ -16,9 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+	
+//listar_cliente
+Route::get('/clientes/lista', 'ClientesController@nomesClientes')->name('cliente_listar');
 
-Route::get('/clientes/lista', 'ClientesController@nomesClientes');
 
-Route::get('/clientes/cadastro', 'ClientesController@cadastro');
+//cadastrar_cliente
+Route::get('/clientes/cadastro', 'ClientesController@cadastro')->name('cliente_cadastrar');
 
+//alterar_cliente
+Route::get('/clientes/alterar/{id}', 'ClientesController@alteracao')->name('cliente_tela_alterar');
+Route::post('/clientes/alterar/{id}', 'ClientesController@alterar')->name('cliente_alterar');
+
+//excluir_cliente
+Route::get('/clientes/excluir/{id}', 'ClientesController@excluir')->name('cliente_excluir');
+
+
+//msg
 Route::post('/clientes/novo', 'ClientesController@novo')->name('cliente_novo');
+
+//cadastrar_venda
+Route::get('/vendas/cadastro', 'VendaController@cadastro')->name("cadastrar_venda");
+Route::post('/vendas/cadastro/nova', 'VendaController@nova')->name("cadastrar_venda_nova");
+
+//listar_vendas
+Route::get('/vendas/listar', 'VendaController@listar')->name("listar_vendas");
+
+
+//excluir_vendas
+Route::get('/vendas/excluir/{id}', 'VendaController@excluir')->name('venda_delete');
+
+//alterar_vendads
+Route::get('/vendas/alterar/{id}', 'VendaController@alteracao')->name('venda_tela_alterar');;
+Route::post('/vendas/alteracao/{id}', 'VendaController@alterar')->name('venda_alterar');
